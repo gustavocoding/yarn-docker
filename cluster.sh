f47992c8d1e3826081fe5b1e2a3ce9e7e88c4f49
gustavonalle <gustavonalle@gmail.com>
2016-02-04 14:28:11 +0000

    Renamed start script and put back number of slaves in the cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+N=${1:-2}
+CUR_DIR="$PWD"
+
 docker run -d --name resolvable --hostname resolvable -v /var/run/docker.sock:/tmp/docker.sock -v /etc/resolv.conf:/tmp/resolv.conf mgood/resolvable
-docker run -d --name master -h master -e "SLAVES=2" gustavonalle/yarn
-docker run -d --link master:master -h slave1 gustavonalle/yarn
-docker run -d --link master:master -h slave2 gustavonalle/yarn
-docker exec -it master sh -c -l '/usr/local/hadoop/sbin/start-all.sh'
+docker run -d --name master -h master -e "SLAVES=$N"  gustavonalle/yarn
+
+START=1
+for (( c=$START; c<=$N; c++)) 
+do
+   docker run -d --link master:master -h slave$c gustavonalle/yarn
+done
+
+docker exec -it master sh -c -l '/usr/local/hadoop/sbin/start-wrapper.sh'
 
 echo  "Cluster started. HDFS UI on http://master:50070/dfshealth.html#tab-datanode"
 echo  "YARN UI on http://master:8088/cluster/nodes"
