ee3acd52daca31f1671693b08e025d25da7486e4
Gustavo Fernandes <gustavonalle@gmail.com>
2018-10-25 16:37:42 +0100

    Use latest versions for the quick start

--- cluster.sh
+++ cluster.sh
@@ -4,12 +4,12 @@ N=${1:-2}
 CUR_DIR="$PWD"
 
 docker run -d --name resolvable --hostname resolvable -v /var/run/docker.sock:/tmp/docker.sock -v /etc/resolv.conf:/tmp/resolv.conf mgood/resolvable
-docker run -d --name master -h master -e "SLAVES=$N"  gustavonalle/yarn:2.7.1
+docker run -d --name master -h master -e "SLAVES=$N"  gustavonalle/yarn
 
 START=1
 for (( c=$START; c<=$N; c++)) 
 do
-   docker run -d --link master:master -h slave$c gustavonalle/yarn:2.7.1
+   docker run -d --link master:master -h slave$c gustavonalle/yarn
 done
 
 sleep 10
